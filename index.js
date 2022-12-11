bfa6ed794e405a3980c6393ff6325a3d72177728
Bharath Kumar <pbkbharath1707@gmail.com>
2022-12-11 16:59:34 -0600

    added operation module

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello Node JS!!")
+const op = require("./operations");
+
+const a = 10;
+const b = 20;
+
+const sum = op.sum(a,b);
+
+console.log("sum: " + sum);
+
+const sub = op.sub(b,a);
+
+console.log("sub: " + sub);
