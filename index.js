2af39a68578314876df65618966cf00c9a1ef183
Bharath Kumar <pbkbharath1707@gmail.com>
2022-12-12 15:05:36 -0600

    added logic for serving html files

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-const http = require('http');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const hostName = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log("req",req.headers);
-    console.log("response: " + res.req);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('Bharath');
+  console.log("req for " + req.url + "by method " + req.method);
 
-})
+  if (req.method === "GET") {
+    var fileUrl;
+    if (req.url === "/") {
+      fileUrl = "/design.html";
+    } else fileUrl = req.url;
+
+    let filePath = path.resolve("./HTML" + fileUrl);
+    const fileExt = path.extname(filePath);
+
+    if (fileExt === ".html") {
+      fs.existsSync(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.end(
+            "<html><body><h1>File Not Found" + fileUrl + "</h1></body></html>"
+          );
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end(
+        "<html><body><h1>File Not an HTML" + fileUrl + "</h1></body></html>"
+      );
+      return;
+    }
+  }else{
+    res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.end(
+            "<html><body><h1>File Format Not supported" + fileUrl + "</h1></body></html>"
+          );
+  }
+});
 
 server.listen(port, hostName, () => {
-console.log(`server listening at http://${hostName}:${port}`);
-})
+  console.log(`server listening at http://${hostName}:${port}`);
+});
