e36139574d492a771f00294d68e99be77fa6cb1c
Bharath Kumar <pbkbharath1707@gmail.com>
2022-12-12 16:52:53 -0600

    added morgan to code

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const http = require("http");
+const morgan = require('morgan');
+
 
 const app = express();
+
+app.use(morgan('dev'));
 
 const hostName = 'localhost';
 const port = 3000;
 
+app.use(express.static(__dirname+'/HTML'))
+
 app.use((req,res,next) => {
-  console.log("req.headers",req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>Express is running</h1></body></html>');
