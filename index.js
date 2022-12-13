678964c938194119e3af9d5c8300e7e1e948b276
Bharath Kumar <pbkbharath1707@gmail.com>
2022-12-13 12:01:01 -0600

    added basic router crud

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ const port = 3000;
 
 app.use(express.static(__dirname+'/HTML'))
 
+
+app.all('/dishes', (req,res,next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  next();
+})
+
+app.get('/dishes', (req, res, next) => {
+  res.end('Dishes data is available');
+});
+
+app.post('/dishes', (req, res, next) => {
+  res.end(`Dishes name: ${req.body.name} with details: ${req.body.details}`);
+});
+
+app.put('/dishes', (req, res, next) => {
+  res.statusCode = 403;
+  res.end("Put Operation is not supported");
+});
+
+app.delete('/dishes', (req, res, next) => {
+  res.end("Deleted Operation");
+});
+
 app.use((req,res,next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
