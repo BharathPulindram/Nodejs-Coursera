142d59f849b5a744df46c7277aa6570a8823732d
Bharath Kumar <pbkbharath1707@gmail.com>
2022-12-15 14:25:46 -0600

    added schema and endpoint logic

--- node-mongo/dbModel.js
+++ node-mongo/dbModel.js
@@ -1,1 +1,11 @@
 import mongoose from 'mongoose';
+
+const schema = mongoose.Schema({
+    userId:Number,
+    id:Number,
+    title:String,
+    data:String,
+})
+
+//collection inside db
+export default mongoose.model('userData',schema);
--- node-mongo/mongooseIndex.js
+++ node-mongo/mongooseIndex.js
@@ -5,20 +5,48 @@ import dotenv from 'dotenv'
 dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
+import Data from "./data.js";
+import UserSchema from "./dbModel.js";
 
 const app = express();
 
+app.use(express.json());
 const port = process.env.PORT || 3000;
 mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.CONNECTION__URL, {
     useNewUrlParser: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
 });
 
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello, world!");
+    res.status(200).send("Bharath");
+})
+
+app.get("/user", (req, res) => {
+    UserSchema.find((err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  });
+
+
+app.post('/user', async (req, res) => {
+    const user = req.body;
+
+    UserSchema.create(user, (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(201).send(data);
+      }
+    });
+
 })
 
 app.listen(port, () => {
