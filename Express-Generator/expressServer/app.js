e1a8ec94a0b6a5d26bc966049807b6db8928e714
Bharath Pulindram <bharath.pulindram@gmail.com>
2022-12-20 09:36:58 -0600

    enabled code formater

--- Express-Generator/expressServer/app.js
+++ Express-Generator/expressServer/app.js
@@ -3,8 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,18 +34,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("1234-5678-9012"));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  })
+);
 
 function auth(req, res, next) {
   console.log("req.headers :", req.headers);
-  console.log("req.session.user",req.session.user)
-  if (!req.session.user){
+  console.log("req.session.user", req.session.user);
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error("You are not authorized");
@@ -53,17 +55,17 @@ function auth(req, res, next) {
       err.status = 401;
       return next(err);
     }
-  
+
     console.log("authHeader :", authHeader);
-  
+
     var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
       .toString()
       .split(":");
     var userName = auth[0];
     var password = auth[1];
-  
+
     if (userName === "admin" && password === "password") {
-      req.session.user = 'admin';
+      req.session.user = "admin";
       next();
     } else {
       var err = new Error("You are not authorized");
@@ -71,18 +73,16 @@ function auth(req, res, next) {
       err.status = 401;
       return next(err);
     }
-  }else{
-    if (req.session.user === 'admin') {
-      console.log('req.session: ',req.session);
+  } else {
+    if (req.session.user === "admin") {
+      console.log("req.session: ", req.session);
       next();
-  }
-  else {
-      var err = new Error('You are not authenticated!');
+    } else {
+      var err = new Error("You are not authenticated!");
       err.status = 401;
       next(err);
+    }
   }
-  }
-  
 }
 
 app.use(auth);
